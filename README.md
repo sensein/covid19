03277fdf05c10f404efb16a180c3d4e6435bfa62
Daniel Mark Low <daniel.m.low@gmail.com>
2020-04-24 12:26:27 -0300

    added covid19 symptom questionnaire and changed path to danielmlow

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ![CI](https://github.com/sensein/covid19/workflows/CI/badge.svg)
 
 # Covid19 voice protocol
+
+Activities:
+* covid19
+* voice
+* voice_opt
+
+
+Test: https://schema.repronim.org/ui/#/?url=https://raw.githubusercontent.com/sensein/covid19/master/protocol/Covid19_schema
--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,5 +1,5 @@
 export default {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/sensein/covid19/master/protocol/Covid19_schema',
-  contentSrc: 'https://raw.githubusercontent.com/sensein/covid19/master/protocol/README.md'
+  githubSrc: 'https://raw.githubusercontent.com/danielmlow/covid19/master/protocol/Covid19_schema',
+  contentSrc: 'https://raw.githubusercontent.com/danielmlow/covid19/master/protocol/README.md'
 }
