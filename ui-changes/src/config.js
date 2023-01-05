30ed1a4f77592f074b5b6ec803fd97f0a91fd71f
prasad <prasad@MAC18J-4dd---Prasad-K>
2023-01-05 09:28:13 -0600

    Revert "edit protocol"
    This reverts commit eb770261e78ad5f42a22d7302d30f1d2f9fa4a6a.

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,10 +1,15 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_4WeekStudy_Screening/main/ScreeningProtocol/ScreeningProtocol_schema',
-  startButton: 'Join'
-  assetsPublicPath: '/MumbleMelody_4WeekStudy_Screening/',
+  githubSrc: 'https://raw.githubusercontent.com/sensein/covid19/master/protocol/Covid19_schema',
+  startButton: {
+    "en": 'Join',
+    "es": 'Participar'
+  },
+  assetsPublicPath: '/covid19/',
   backendServer: 'https://sig.mit.edu/vb',
   showHelp: true,
+  contact: 'voice_survey@mit.edu',
+  emailSubject: 'Help with Covid19 study',
   checkMediaPermission: true
 };
 
